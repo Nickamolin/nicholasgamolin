dd37b867c0f1bf9daaac3ece5de73e8368a96d1c
Nicholas Gamolin <asr8gn@virginia.edu>
2025-10-07 13:57:40 -0400

    added full name in bold, added 'see my art' button, customized 'see my videos' button

--- app/page.js
+++ app/page.js
@@ -15,6 +15,10 @@ export default function Home() {
           priority
         /> */}
 
+        <a className="text-4xl font-bold">
+          Nicholas Gamolin
+        </a>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -32,12 +36,20 @@ export default function Home() {
             see my code
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://www.youtube.com/@Drakonic"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center bg-gray-900 hover:bg-gray-800  hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto"
+            href="https://www.youtube.com/@Drakonic/videos"
             target="_blank"
             rel="noopener noreferrer"
           >
             see my videos
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto"
+            href="https://negativedomain.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            see my art
           </a>
         </div>
     </div>

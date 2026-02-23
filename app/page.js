0216fae4ea70991e582f9e8ddb5ea54d45b64626
Nicholas Gamolin <asr8gn@virginia.edu>
2026-02-23 16:24:50 -0500

    moved linkedin logo to proper button

--- app/page.js
+++ app/page.js
@@ -8,25 +8,26 @@ export default function Home() {
 
       <div className="w-full flex flex-row justify-center items-center gap-2">
         <span className="text-3xl sm:text-4xl font-bold text-center w-auto">Nicholas Gamolin</span>
-        <a
-          href="https://www.linkedin.com/in/nicholas-gamolin/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="LinkedIn Profile"
-        >
-          <img
-            src="/linkedinBlack.svg"
-            width="27"
-            height="27"
-            alt="LinkedIn"
-            className="dark:invert transition-opacity hover:opacity-75 w-[27px] h-[27px] sm:w-[34px] sm:h-[34px]"
-          />
-        </a>
       </div>
 
       <div className="flex gap-4 items-center flex-col sm:flex-row">
         <a
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto gap-2"
+          href="https://www.linkedin.com/in/nicholas-gamolin/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+          <img
+            src="/linkedinBlack.svg"
+            width="23"
+            height="23"
+            alt="LinkedIn"
+            className="invert dark:invert-0 w-[23px] h-[23px] sm:w-[28px] sm:h-[28px]"
+          />
+        </a>
+        <a
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center text-background bg-gray-700 hover:bg-gray-600 dark:bg-gray-400 dark:hover:bg-gray-500 hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto gap-2"
           href="https://github.com/Nickamolin"
           target="_blank"
           rel="noopener noreferrer"
@@ -56,7 +57,7 @@ export default function Home() {
           />
         </a>
         <a
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto gap-2" 
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto gap-2"
           href="https://negativedomain.com/"
           target="_blank"
           rel="noopener noreferrer"

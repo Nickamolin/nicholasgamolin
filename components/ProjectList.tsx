0d6768f0d154ecff530757465516feb5afa3327f
Nickamolin <asr8gn@virginia.edu>
2026-02-25 16:22:18 -0500

    added rendering of images with project metadata

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
 export default async function ProjectList() {
@@ -6,7 +7,7 @@ export default async function ProjectList() {
 
     const { data: projects, error } = await supabase
         .from("projects")
-        .select("id, name")
+        .select("id, name, thumbnail_url")
         .order("id", { ascending: true });
 
     if (error) {
@@ -14,12 +15,13 @@ export default async function ProjectList() {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center w-full max-w-4xl">
+        <div className="flex flex-col items-center justify-center w-full">
             <h1 className="p-6 text-3xl font-bold">Projects:</h1>
-            <ul>
+            <ul className="flex flex-row items-end justify-center gap-4">
                 {projects?.map((project: any) => (
-                    <li key={project.id}>
-                        {project.id}: <code>{project.name}</code>
+                    <li key={project.id} className="flex flex-col items-center justify-end px-4">
+                        <Image src={project.thumbnail_url} alt={project.name} width={200} height={200} />
+                        <p className="text-xl font-bold">{project.id}. <code>{project.name}</code></p>
                     </li>
                 ))}
             </ul>

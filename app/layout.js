1ccd28069725efdbe2ec0f55345aa19a825daf84
Nickamolin <asr8gn@virginia.edu>
2026-03-03 18:39:59 -0500

    added vercel analytics for tracking web traffic in vercel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LoadingScreen from "@/components/LoadingScreen";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       >
         <LoadingScreen />
         {children}
+        <Analytics />
       </body>
     </html>
   );

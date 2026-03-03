937daaa1f31e12e0242b3008484fd835a8cd70f3
Nickamolin <asr8gn@virginia.edu>
2026-03-03 18:44:43 -0500

    added analytics to packages and replaced <img> tags with Next's <Image> for better build output

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Logo3D from "@/components/Logo3D";
 import Projects from "@/components/projects/Projects";
+import Image from "next/image";
 
 export default async function Home() {
 
@@ -17,10 +18,10 @@ export default async function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img
+              <Image
                 src="/linkedinBlack.svg"
-                width="50"
-                height="50"
+                width={50}
+                height={50}
                 alt="LinkedIn"
                 className="dark:invert hover:opacity-75 transition-all duration-500 w-[24px] h-[24px] sm:w-[24px] sm:h-[24px]"
               />
@@ -30,10 +31,10 @@ export default async function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img
+              <Image
                 src="/github.svg"
-                width="50"
-                height="50"
+                width={50}
+                height={50}
                 alt="GitHub"
                 className="dark:invert hover:opacity-75 transition-all duration-500 w-[24px] h-[24px] sm:w-[24px] sm:h-[24px]"
               />
@@ -43,10 +44,10 @@ export default async function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img
+              <Image
                 src="/YouTubeIconBW.svg"
-                width="50"
-                height="50"
+                width={50}
+                height={50}
                 alt="YouTube"
                 className="dark:invert hover:opacity-75 transition-all duration-500 w-[24px] h-[24px] sm:w-[24px] sm:h-[24px]"
               />

63737a138cda0f5e4b9e4bf303c94551dd0f974c
Nickamolin <asr8gn@virginia.edu>
2026-04-07 15:35:52 -0400

    brought highlighted projects down to 4

--- components/projects/Highlights.tsx
+++ components/projects/Highlights.tsx
@@ -8,7 +8,7 @@ export default async function Highlights() {
     const { data: projects, error } = await supabase
         .from("projects")
         .select("id, title, subtitle, thumbnail_url, date_published, info_url, visible, type, embed_url, embed_type, embed_aspect_ratio, hover_text, render_title")
-        .in("id", [1, 2, 3, 6, 9])
+        .in("id", [1, 2, 3, 6]) // 1: Endangered Cake Museum, 2: Brick Digger, 3: UBlock, 6: Get to Know Flourish
         .order("date_published", { ascending: false });
 
     if (error) {
@@ -17,7 +17,7 @@ export default async function Highlights() {
 
     return (
         <div className="w-full max-w-[2000px] flex flex-col items-center">
-            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full max-w-[2000px] gap-4">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 w-full max-w-[2000px] gap-4">
                 {projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}

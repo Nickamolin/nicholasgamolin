ce3f4e8af6e4d6205148c586c2ab28c0b218a591
Nickamolin <asr8gn@virginia.edu>
2026-04-23 13:52:51 -0400

    changed phrasing on contact card

--- components/sections/ContactCard.tsx
+++ components/sections/ContactCard.tsx
@@ -48,7 +48,7 @@ export default function ContactCard() {
         <div className="flex flex-col items-center justify-center w-full mb-[5vh]">
             <div className="flex flex-col items-center gap-(--spacing-header-gap) mb-(--spacing-header-mb)">
                 <h1 className="text-4xl sm:text-5xl md:text-7xl font-title font-bold">Contact Me</h1>
-                <span className="text-xs md:text-sm font-subtitle font-medium text-center text-gray-400 tracking-[0.2em] uppercase">Have an idea or just want to say hi?</span>
+                <span className="text-xs md:text-sm font-subtitle font-medium text-center text-gray-400 tracking-[0.2em] uppercase">Open to new projects, ideas, and conversations</span>
             </div>
 
             <div className="w-full max-w-lg p-8 rounded-3xl border border-white/10 bg-black/5 backdrop-blur-xl">
